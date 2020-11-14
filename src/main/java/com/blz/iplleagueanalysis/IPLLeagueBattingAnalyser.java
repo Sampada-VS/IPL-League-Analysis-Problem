f666a7d111ae6212862cd2e6d7ea812755ed0dee
Sampada-VS <samssv1@gmail.com>
2020-11-15 00:35:16 +0530

    Refactored class names

--- src/main/java/com/blz/iplleagueanalysis/IPLLeagueBattingAnalyser.java
+++ src/main/java/com/blz/iplleagueanalysis/IPLLeagueBattingAnalyser.java
@@ -6,15 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-public class IPLLeagueAnalyser {
+public class IPLLeagueBattingAnalyser {
 	static List<CSVMostRuns> csvRuns;
-	static List<CSVMostWickets> csvWickets;
-
+	
 	public int loadMostRunsCSV(String csvFilePath) {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			CsvToBean<CSVMostRuns> csvToBean = new CsvToBeanBuilder(reader).withType(CSVMostRuns.class)
@@ -24,17 +22,6 @@ public class IPLLeagueAnalyser {
 			e.printStackTrace();
 		}
 		return csvRuns.size();
-	}
-
-	public int loadMostWicketsCSV(String csvFilePath) {
-		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-			CsvToBean<CSVMostWickets> csvToBean = new CsvToBeanBuilder(reader).withType(CSVMostWickets.class)
-					.withIgnoreLeadingWhiteSpace(true).build();
-			csvWickets = csvToBean.parse();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return csvWickets.size();
 	}
 
 	public double getTopBattingAverage(String csvFilePath) {
--- src/test/java/com/blz/iplleagueanalysis/IPLLeagueBattingAnalyserTest.java
+++ src/test/java/com/blz/iplleagueanalysis/IPLLeagueBattingAnalyserTest.java
@@ -2,81 +2,70 @@ package com.blz.iplleagueanalysis;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class IPLLeagueAnalyserTest {
+public class IPLLeagueBattingAnalyserTest {
 
-	static IPLLeagueAnalyser iPLLeagueAnalyser;
+	static IPLLeagueBattingAnalyser iPLLeagueBattingAnalyser;
 	private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "src/IPL2019FactsheetMostRuns.csv";
-	private static final String IPL_MOST_WICKETS_CSV_FILE_PATH = "src/IPL2019FactsheetMostRuns.csv";
 
 	@BeforeClass
 	public static void createObj() {
-		iPLLeagueAnalyser = new IPLLeagueAnalyser();
+		iPLLeagueBattingAnalyser = new IPLLeagueBattingAnalyser();
 	}
 
 	@AfterClass
 	public static void nullObj() {
-		iPLLeagueAnalyser = null;
+		iPLLeagueBattingAnalyser = null;
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_ShouldReturnCorrectRecords() {
-		int numOfRecords = iPLLeagueAnalyser.loadMostRunsCSV(IPL_MOST_RUNS_CSV_FILE_PATH);
-		assertEquals(100, numOfRecords);
-	}
-
-	@Test
-	public void givenIPLMostWicketsCSVFile_ShouldReturnCorrectRecords() {
-		int numOfRecords = iPLLeagueAnalyser.loadMostWicketsCSV(IPL_MOST_WICKETS_CSV_FILE_PATH);
+		int numOfRecords = iPLLeagueBattingAnalyser.loadMostRunsCSV(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals(100, numOfRecords);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenTopBattingAverage_ShouldReturnCorrectRecord() {
-		double topBattingAverage = iPLLeagueAnalyser.getTopBattingAverage(IPL_MOST_RUNS_CSV_FILE_PATH);
+		double topBattingAverage = iPLLeagueBattingAnalyser.getTopBattingAverage(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals(83.2, topBattingAverage, 0.0);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenTopStrikingRate_ShouldReturnCorrectRecord() {
-		String topStrikingRate = iPLLeagueAnalyser.getTopStrikingRate(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String topStrikingRate = iPLLeagueBattingAnalyser.getTopStrikingRate(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("333.33,Ishant Sharma", topStrikingRate);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenMax6s_ShouldReturnCricketer() {
-		String max6sHit = iPLLeagueAnalyser.getCricketerWhoHitmax6s(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String max6sHit = iPLLeagueBattingAnalyser.getCricketerWhoHitmax6s(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("52,Andre Russell", max6sHit);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenMax4s_ShouldReturnCricketer() {
-		String max4sHit = iPLLeagueAnalyser.getCricketerWhoHitmax4s(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String max4sHit = iPLLeagueBattingAnalyser.getCricketerWhoHitmax4s(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("64,Shikhar Dhawan", max4sHit);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenBestStrikingRate_ShouldReturnCricketerWithHis6sAnd4s() {
-		String bestStrikeRateCricketer = iPLLeagueAnalyser
-				.getCricketerWhoHasBestStrikingRate(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String bestStrikeRateCricketer = iPLLeagueBattingAnalyser.getCricketerWhoHasBestStrikingRate(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("333.33,1,1", bestStrikeRateCricketer);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenGreatAverage_ShouldReturnCricketerWithStrikeRate() {
-		String bestAverageCricketer = iPLLeagueAnalyser.getCricketerWhoHasBestAverage(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String bestAverageCricketer = iPLLeagueBattingAnalyser.getCricketerWhoHasBestAverage(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("MS Dhoni,83.2,134.62", bestAverageCricketer);
 	}
 
 	@Test
 	public void givenIPLMostRunsCSVFile_WhenMaxRuns_ShouldReturnCricketerWithBestAverage() {
-		String maxRunsScorer = iPLLeagueAnalyser.getCricketerWhoHitMaxRuns(IPL_MOST_RUNS_CSV_FILE_PATH);
+		String maxRunsScorer = iPLLeagueBattingAnalyser.getCricketerWhoHitMaxRuns(IPL_MOST_RUNS_CSV_FILE_PATH);
 		assertEquals("David Warner ,692,69.2", maxRunsScorer);
 	}
 
